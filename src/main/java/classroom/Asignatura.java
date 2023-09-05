0d2c654480629a3ae56e244a724837aa5d874bcf
NICOXO <joduquel@unal.edu.co>
2023-09-04 21:57:42 -0500

    taller4

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -1,39 +1,60 @@
 package classroom;
 
 public class Asignatura {
-    private String nombre;
-    private int codigoInterno;
-    private int codigoExterno;
-    private Tipo tipo;
+
+    public String nombre;
+    String n = nombre;
+    public int codigoInterno;
+    public int codigoExterno;
+    public Tipo tipo;
 
     public Asignatura() {
-        this("Sin nombre", 0, 0, Tipo.FUNDAMENTACION);
+        this("Sin nombre", Tipo.FUNDAMENTACION);
+    }
+
+    public Asignatura(int codigoInterno,Tipo tipo) {
+        this("Sin nombre", codigoInterno, 0,tipo);
+    }
+
+    public Asignatura(int codigoExterno,Tipo tipo) {
+        this("Sin nombre", 0, codigoExterno,tipo);
     }
 
     public Asignatura(String nombre, Tipo tipo) {
-        this(nombre, 0, 0, tipo);
+        this(nombre, 0, 0,tipo);
     }
 
-    public Asignatura(String nombre, int codigoInterno, int codigoExterno, Tipo tipo) {
+    public Asignatura(String nombre, int codigoInterno, int codigoExterno,Tipo tipo) {
         this.nombre = nombre;
         this.codigoInterno = codigoInterno;
         this.codigoExterno = codigoExterno;
         this.tipo = tipo;
     }
 
-    public String getNombre() {
-        return nombre;
+    public void cambiarDatos(int codigoInterno, int codigoExterno, String nombre,Tipo tipo) {
+        this.codigoInterno = codigoInterno;
+        this.codigoExterno = codigoExterno;
+        this.nombre = nombre;
+        this.tipo = tipo;
     }
 
-    public int getCodigoInterno() {
-        return codigoInterno;
+    public void cambiarDatos(int codigoInterno) {
+        this.codigoInterno = codigoInterno;
     }
 
-    public int getCodigoExterno() {
-        return codigoExterno;
+    public void cambiarDatos(int codigoExterno) {
+        this.codigoExterno = codigoExterno;
     }
 
-    public Tipo getTipo() {
-        return tipo;
+    public void cambiarDatos(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCodigoInterno(int codigoInterno) {
+        this.codigoInterno = codigoInterno;
+    }
+    
+    public void setCodigoInterno(double codigoInterno) {
+        this.codigoInterno = (int) codigoInterno;
     }
 }
--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,5 +1,11 @@
-package classroom;
-
 public enum Tipo {
-    DISCIPLINAR, FUNDAMENTACION, ELECTIVA;
+    DISCIPLINAR(10),
+    FUNDAMENTACION(20),
+    ELECTIVA(30);
+
+    private int codigo;
+
+    Tipo(int codigo) {
+        this.codigo = codigo;
+    }
 }
